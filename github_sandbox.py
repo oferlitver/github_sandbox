d1937d8424ae45e6be419e3121c770d64a05f4f6
Ofer Litver <oferl@sentinelone.com>
2018-11-30 18:32:44 +0200

    refactored to main() function

--- github_sandbox.py
+++ github_sandbox.py
@@ -2,6 +2,11 @@ import os
 import numpy as np
 
 # this is a new comment
-print os.getcwd()
-mat = np.array([[1, 2],
-                [3, 4]])
+def main():
+    print os.getcwd()
+    mat = np.array([[1, 2],
+                    [3, 4]])
+    print mat.transpose()
+
+if __name__ == '__main__':
+    main()
